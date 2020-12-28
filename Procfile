77464e20c318c38d8211d2f4ab0be7d04fe99504
tomasrpons <tomasrpons@gmail.com>
2020-12-28 14:14:22 -0300

    tweaked app for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rivera_padel.wsgi
--- rivera_padel/settings.py
+++ rivera_padel/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'rivera_padel.urls'
@@ -113,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -121,3 +127,4 @@ STATICFILES_DIRS = [
 
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
+django_heroku.settings(locals())
